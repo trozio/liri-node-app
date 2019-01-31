a36ab1b2a8b033f2a445f92e8d993f4f8a8cc532
trozio <daniel.l.adelman@gmail.com>
2019-01-31 18:22:05 -0500

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
-
-
 let axios = require("axios");
+let dotenv = require("dotenv").config();
+let Spotify = require('node-spotify-api');
+var moment = require('moment');
+moment().format();
 
  let command = process.argv[2];
  let question = process.argv[3];
@@ -9,13 +10,24 @@ let axios = require("axios");
 	if(command === "concert-this"){
   axios.get("https://rest.bandsintown.com/artists/" + question + "/events?app_id=codingbootcamp").then(
     function(response) {
-      console.log(response.name);
+      console.log("Upcoming concert: " + response.data[0].venue.name + ", " + response.data[0].venue.city + ", " + response.data[0].venue.country + moment(response.data[0].datetime).format());
     }
-  );
+)};
 
-	}
+
 	if(command[2] === "spotify-this-song"){
 
+
+var spotify = new Spotify(keys.spotify);
+
+spotify
+  .request('https://api.spotify.com/v1/tracks/' + question + '/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err);
+  });
 	}
 	if(command[2] === "movie-this"){
 
