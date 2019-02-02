4c648ff1f2d625e8940e318eba3e249b9e1080f1
trozio <daniel.l.adelman@gmail.com>
2019-02-02 10:35:08 -0500

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,36 +1,48 @@
-let axios = require("axios");
-let dotenv = require("dotenv").config();
+require("dotenv").config();
+let keys = require("./keys.js")
+let fs = require("fs");
 let Spotify = require('node-spotify-api');
-var moment = require('moment');
+let spotify = new Spotify(keys.spotify);
+let moment = require('moment');
+
 moment().format();
 
- let command = process.argv[2];
- let question = process.argv[3];
+let command = process.argv[2];
+let question = process.argv[3];
 
-	if(command === "concert-this"){
-  axios.get("https://rest.bandsintown.com/artists/" + question + "/events?app_id=codingbootcamp").then(
-    function(response) {
-      console.log("Upcoming concert: " + response.data[0].venue.name + ", " + response.data[0].venue.city + ", " + response.data[0].venue.country + " " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
-    }
-)};
-
-
-	if(command[2] === "spotify-this-song"){
+if (command === "concert-this") {
+	axios.get("https://rest.bandsintown.com/artists/" + question + "/events?app_id=codingbootcamp").then(
+		function(response) {
+			console.log("Upcoming concert: " + response.data[0].venue.name + ", " + response.data[0].venue.city + ", " + response.data[0].venue.country + " " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
+		}
+	)
+};
 
 
-var spotify = new Spotify(keys.spotify);
+if (command[2] === "spotify-this-song") {
+	spotifySong();
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+}
+if (command[2] === "movie-this") {
 
-console.log(data);
-});
-	}
-	if(command[2] === "movie-this"){
+}
+if (command[2] === "do-what-it-says") {
 
-	}
-	if(command[2] === "do-what-it-says"){
+}
 
-	}
+
+
+function spotifySong(question) {
+
+	spotify.search({
+		type: 'track',
+		query: question,
+		limit: 5
+	}, function(err, data) {
+		if (err) {
+			console.log("Error: " + err);
+		}
+
+		console.log(data);
+	});
+}
